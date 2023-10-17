32db97186c92a75c37902c59563c3c884fa79b94
woldejamawolde <wolde.jama@m0ringaschool.com>
2023-10-17 21:20:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 function addingEventListener() {
 }
+function addingEventListener() {
+   
+    const input = document.getElementById('input');
+    input.addEventListener('input', addingEventListener); alert('I was clicked!');
+    
+    }
+    //const input = document.getElementById('input');
+    //input.addEventListener('input', addingEventListener);
+    /*const input = document.getElementById('input');
+    
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+    
+    input.addEventListener('click', clickAlert); */
